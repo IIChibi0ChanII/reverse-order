97ce70ff103782897995db0d0c338e4d1fb91303
IIChibi0ChanII <mahamudashanta@gmail.com>
2025-05-31 18:45:49 +0600

    comments

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ n = int(input("Enter a random string of numbers: "))
 num = n
 res = 0
 while num != 0:
-    a = num%10
-    res = res * 10 + a
-    num = num // 10
+    a = num%10 # gets the last number
+    res = res * 10 + a # adds a 0 at the end of the result and replaces with the last number
+    num = num // 10 #excludes the last number for the next calculation
 
 print(res)
